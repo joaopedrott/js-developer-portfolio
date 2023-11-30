5ff691acd2c320b083ae7a310756cf2f277faac7
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-11-30 18:02:59 -0300

    danimazando as imagens das hardskills

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,13 +30,17 @@ function updateSoftSkills(profileData) {
 
 }
 
-function updateImg(profileData) {
-    
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+
+
 }
 
 (async function () {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
+    updateHardSkills(profileData)
     //console.log(profileData)
 })()
