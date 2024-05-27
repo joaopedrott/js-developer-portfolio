90936158e9e0eb6c6e9a4aa988c6097611b4414e
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-05-26 21:58:22 -0300

    correcao

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,7 @@ function updatePortfolio(profileData) {
         <li>
         <h3 ${project.github ? 'class="github"': ''}>${project.name}</h3>
         <a href="${project.url}" target="_blank">${project.url}</a>
-        <p>Descricao: ${profileData.descricao}</p>
+        <p>Descricao: ${project.descricao2}</p>
         </li>`
     }).join('')
 }
